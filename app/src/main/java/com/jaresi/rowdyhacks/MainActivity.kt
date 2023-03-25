48b5a633fa6d9671f6ce3d3ae3d2a061ca84fd4d
Jaresi Nunez <58448481+jaresinunez@users.noreply.github.com>
2023-03-25 16:40:15 -0500

    image fetch from internet

--- app/src/main/java/com/jaresi/rowdyhacks/MainActivity.kt
+++ app/src/main/java/com/jaresi/rowdyhacks/MainActivity.kt
@@ -2,10 +2,34 @@ package com.jaresi.rowdyhacks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.picasso.Picasso
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
+import android.util.Log
+import android.widget.ImageView
+import android.widget.Toast
+import coil.imageLoader
+import coil.request.ImageRequest
+import com.jaresi.rowdyhacks.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val request = ImageRequest.Builder(this)
+            .data("https://www.travelandleisure.com/thmb/sH4T0ElWwZFyUhtqAZD3USeiaSc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/james-webb-hole-WEBB0722-a7b14258290d4da9be4c7d50ee732d9b.jpg")
+            .crossfade(true)
+            .target(binding.astronomyPod)
+            .build()
+
+        imageLoader.enqueue(request)
+
+        setContentView(binding.root)
     }
 }
